74d1655da6448c0231f103b460abfe921b1abf8e
FelypeCl <fefeclayton2@gmail.com>
2022-08-23 17:03:43 -0300

    Rework em todo script do progra + Animações + Farm

--- src/Launcher.js
+++ src/Launcher.js
@@ -1,80 +1,74 @@
 document.addEventListener('DOMContentLoaded', () => {
     removerDOMS();
     iniciar();
+    iniciarFarmAlgodao();
 })
 
+/*
+    Objetos
+*/
+
+const PLAYER = new Player(1000, 0, 0);
+const TERRENO_BLOQUEADO = new Planta("Bloqueado", 0, 0, 0, 0, "../assets/terraBloqueada.png");
+const ALGODAO = new Planta("Algodao", 5000, 5, 35, 1, "../assets/algodao_grande.png");
+
+//Terreno Inicial
+TERRENO_BLOQUEADO.gerarTerreno();
+
 function iniciar(){
-    const PLAYER = new Player(1000, 0, 0);
-    const TERRENO_BLOQUEADO = new Planta("Bloqueado", 0, 0, 0, 0, "../assets/terraBloqueada.png");
-    const ALGODAO = new Planta("Algodao", 5, 5, 5, 5, "../assets/algodao_grande.png");
-    var meusTerrenos = [];
-    
-    //Terreno Inicial
-    criarLieImgsTerreno(TERRENO_BLOQUEADO);
-    comprarPlanta();
+    const $TERRENO_PRA_COMPRA = document.getElementById("plantali"+meusTerrenos.length);
+
+    $TERRENO_PRA_COMPRA.onclick = () => {
+        comprarPlanta();
+    };
 
     function comprarPlanta(){
-        let localizarTerreno = document.getElementById("planta" + meusTerrenos.length);
-        localizarTerreno.addEventListener('click', () => {
-            abrirLojaDePlantas();
-            const $PLANTA_VENDA_1 = document.getElementById("plantaVenda1");
-            $PLANTA_VENDA_1.addEventListener("click", () => {
-                if(PLAYER.dinheiro >= 10){
-                    PLAYER.dinheiro -= 10;
-                    fecharLojaDePlantas();
-                    removerLieImgsTerreno();
-                    criarLieImgsTerreno(ALGODAO);
-                    criarLieImgsTerreno(TERRENO_BLOQUEADO);
-                    console.log("planta" + meusTerrenos.length);
-                    console.log(meusTerrenos);
-                    comprarPlanta();
-                } else {
-                    console.log("Sem dinheiro");
+        abrirLojaDePlantas();
+        const $PLANTA_VENDA = document.getElementById("plantaVenda1");
+        $PLANTA_VENDA.onclick = () => {
+            if(PLAYER.dinheiro >= 10){
+                PLAYER.dinheiro -= 10;
+                PLAYER.xp += ALGODAO.qntdXp;
+                for(let i = 0; i < meusTerrenos.length; i++){
+                    if(meusTerrenos[i].tipoDaPlanta == "Bloqueado"){
+                        meusTerrenos[i].removerTerreno();
+                    }
                 }
-            });
-        });
-    }
-
-    function criarLieImgsTerreno(terreno){
-        meusTerrenos.push(terreno); //Guardar o terreno na lista de terreno.
-        const REDE_DE_PLANTAS = document.getElementById("redeDePlantas");
-        let criarLi = document.createElement("li");
-        let criarImg = document.createElement("img");
-        let localizarTerreno = meusTerrenos.length;
-        criarLi.setAttribute("id","plantali" + localizarTerreno);
-        criarLi.setAttribute("class","planta");
-        criarImg.setAttribute("id", "planta" + localizarTerreno);
-        criarImg.setAttribute("class", "plantas");
-        criarImg.setAttribute("src", terreno.asset);
-        REDE_DE_PLANTAS.appendChild(criarLi).appendChild(criarImg);
-    }
-
-    function removerLieImgsTerreno(){
-        const REDE_DE_PLANTAS = document.getElementById("redeDePlantas");
-        let localizarTerreno = meusTerrenos.length;
-        let li = document.getElementById("plantali" + localizarTerreno);
-        REDE_DE_PLANTAS.removeChild(li);
-
-        if(meusTerrenos.length <= 1){
-            meusTerrenos.splice(0,1);
-        } else {
-            meusTerrenos.splice(0, meusTerrenos.length);
-        }
-    }
-
-    function carregarTerrenos(){
-        meusTerrenos.forEach((terreno) => {
-            //console.log(terreno)
-        });
-
-        //console.log("Terrenos Atualizados: Quantidade de terrenos = " + meusTerrenos.length);
-        setTimeout(carregarTerrenos, TEMPO_DE_UPDATE_CLIENT);
-    }; carregarTerrenos();
+                ALGODAO.gerarTerreno();
+                TERRENO_BLOQUEADO.gerarTerreno();
+                console.log(meusTerrenos)
+                fecharLojaDePlantas();
+            } else {
+                console.log("Você não tem dinheiro.");
+            }
+        };
+    };
 
     function atualizarDadosUser(){
         console.log('Dados do usuário atualizadas.');
         document.getElementById("dinheiro").innerHTML = "R$"+PLAYER.dinheiro;
-        document.getElementById("xp").innerHTML = "XP:"+PLAYER.level;
-        setTimeout(atualizarDadosUser, TEMPO_DE_UPDATE_CLIENT);
+        document.getElementById("xp").innerHTML = "XP:"+PLAYER.xp;
     }; atualizarDadosUser();
+
+    setTimeout(iniciar, TEMPO_DE_UPDATE_CLIENT);
+}
+
+function iniciarFarmAlgodao(){
+    if(meusTerrenos.length < 1)
+        return;
+
+    let qntdTerrasAlgodao = 0;
+
+    for(let i = 0; i < meusTerrenos.length; i++){
+        if(meusTerrenos[i].tipoDaPlanta == "Algodao"){
+            qntdTerrasAlgodao += 1;
+        }
+    }
+
+    PLAYER.dinheiro += qntdTerrasAlgodao * ALGODAO.qntdDinheiro;
+
+    console.log("Qntd algodao: ", qntdTerrasAlgodao)
+    console.log("Você faturou: ", qntdTerrasAlgodao * ALGODAO.qntdDinheiro, " com algodão")
+
+    setTimeout(iniciarFarmAlgodao, ALGODAO.tempoDeColheita);
 }
--- src/plantas/Planta.js
+++ src/plantas/Planta.js
@@ -4,6 +4,7 @@ var qntdXp;
 var qntdDinheiro;
 var nivelDaPlanta
 var asset;
+var meusTerrenos = [];
 
 const tiposDePlantas = ["Bloqueado", "Algodao", "Alho", "Batata", "Cebola"]
 
@@ -15,5 +16,44 @@ class Planta{
         this.qntdDinheiro = qntdDinheiro;
         this.nivelDaPlanta = nivelDaPlanta;
         this.asset = asset;
+    }
+
+    gerarTerreno(){
+        meusTerrenos.push(this);
+        const REDE_DE_PLANTAS = document.getElementById("redeDePlantas");
+        let criarLi = document.createElement("li");
+        let criarImg = document.createElement("img");
+        let criarTxt = document.createElement("h3");
+        let texto = document.createTextNode("R$35");
+        let localizarTerreno = meusTerrenos.length;
+        criarLi.setAttribute("id","plantali" + localizarTerreno);
+        criarLi.setAttribute("class","planta");
+        criarImg.setAttribute("id", "planta" + localizarTerreno);
+        criarImg.setAttribute("class", "plantas");
+        criarImg.setAttribute("src", this.asset);
+        criarTxt.appendChild(texto);
+        criarTxt.setAttribute("id", "dinheiroli" + localizarTerreno);
+        criarTxt.setAttribute("class", "dinheiroProfit");
+        
+        if(meusTerrenos.length == 1 && this.tipoDaPlanta == "Bloqueado"){
+            REDE_DE_PLANTAS.appendChild(criarLi).appendChild(criarImg);
+        } else if (meusTerrenos[meusTerrenos.length - 1].tipoDaPlanta == "Bloqueado"){
+            REDE_DE_PLANTAS.appendChild(criarLi).appendChild(criarImg);
+        } else {
+            REDE_DE_PLANTAS.appendChild(criarLi).appendChild(criarImg);
+            REDE_DE_PLANTAS.appendChild(criarLi).appendChild(criarTxt);
+        }
+    }
+
+    removerTerreno(){
+        const REDE_DE_PLANTAS = document.getElementById("redeDePlantas");
+        let li = document.getElementById("plantali" + meusTerrenos.length);
+        REDE_DE_PLANTAS.removeChild(li);
+
+        if(meusTerrenos.length == 1){
+            meusTerrenos.shift();
+        } else {
+            meusTerrenos.pop();
+        }
     }
 }
